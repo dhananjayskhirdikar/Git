21fcdf33bcd33008a13346aadeaa7c03a37b964a
DK <dhananjaykhirdikar3@gmail.com>
2025-11-25 20:14:42 +0530

    work

--- Terraform/Provider_Resources/Resource.tf
+++ Terraform/Provider_Resources/Resource.tf
@@ -1,5 +1,6 @@
-resource "aws_instance" "example"
+resource "aws_instance" "example" {
    instance_type = "t3.micro"
    ami = "ami-0d176f79571d18a8f"
    key_name = "dk"
    vpc_security_group_id = ["sg-0f2758b7b37e072cf"]
+}
