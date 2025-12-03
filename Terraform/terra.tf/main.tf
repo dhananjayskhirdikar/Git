faf1edd01a8ff2fbbc63b6f9a72bf1ba3ca23f01
DK <dhananjaykhirdikar3@gmail.com>
2025-12-03 16:22:10 +0530

    new

--- Terraform/terra.tf/main.tf
+++ Terraform/terra.tf/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webserver" {
-    instance_type = "t3.micro"
-    ami = "ami-0f00d706c4a80fd93"
-    key_name = "mykey"
-    vpc_security_group_ids = ["sg-0b4ace81d68dd0b6d"]
-    disable_api_termination = true
-    count = 4  
+    instance_type = var.webserver_instance_type
+    ami = var.webserver_ami
+    key_name = var.webserver_key_name
+    vpc_security_group_ids = [var.webserver_vpc_security_group_ids]
+    disable_api_termination = var.webserver_disable_api_termination
+    count = var.webserver_count
 }
