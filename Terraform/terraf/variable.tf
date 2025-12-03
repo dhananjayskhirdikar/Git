405498e0c467ee2c06cd73ccf2872fccd31c5395
DK <dhananjaykhirdikar3@gmail.com>
2025-12-03 16:44:05 +0530

    new

--- Terraform/terraf/variable.tf
+++ Terraform/terraf/variable.tf
@@ -1,18 +1,18 @@
 variable "webserver_instance_type" {
-    default = "t3.small"
+  default = "t3.small"
 }
 variable "webserver_ami" {
-    default = "ami-0fa3fe0fa7920f68e"
+  default = "ami-0f00d706c4a80fd93"
 }
 variable "webserver_key_name" {
-    default = "octa"
+  default = "mykey"
 }
 variable "webserver_vpc_security_group_ids" {
-    default = "sg-045bfc89f8b1df27c"
+  default = "sg-0b4ace81d68dd0b6d"
 }
 variable "webserver_disable_api_termination" {
-    default = false
+  default = true
 }
 variable "webserver_count" {
-    default = 3
+  default = 2
 }
