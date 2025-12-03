e4466725b65a26896d1ab947b89105b2c7541754
DK <dhananjaykhirdikar3@gmail.com>
2025-12-03 15:44:05 +0530

    new

--- Terraform/terra.tf/main.tf
+++ Terraform/terra.tf/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "webserver" {
-    instance_type = "t3.large"
+    instance_type = "t3.micro"
     ami = "ami-0f00d706c4a80fd93"
     key_name = "mykey"
     vpc_security_group_ids = ["sg-0b4ace81d68dd0b6d"]
