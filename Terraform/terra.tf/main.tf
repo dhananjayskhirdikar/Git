5be4a8a8bae06485bc645a71f30398fdb8a8d02c
DK <dhananjaykhirdikar3@gmail.com>
2025-12-03 15:35:02 +0530

    new

---
+++ Terraform/terra.tf/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "webserver" {
+    instance_type = "t3.large"
+    ami = "ami-0f00d706c4a80fd93"
+    key_name = "mykey"
+    vpc_security_group_ids = ["sg-0b4ace81d68dd0b6d"]
+    disable_api_termination = true
+    count = 4  
+}
